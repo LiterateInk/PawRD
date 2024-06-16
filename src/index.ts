a398c6bd894f8d15b77d8558d63999994aa47d5d
Mikkel RINGAUD <mikkel@milescode.dev>
2024-06-17 01:38:22 +0200

    chore: add exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-export {};
+// TODO: Move inside the fetcher.
+process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+
+export { Authenticator } from "./models/Authenticator";
+export { Client } from "./models/Client";
+
+export { getOnlinePayments, type OnlinePayments, type WalletData, type PaymentConfiguration, type User } from "./api/online-payments";
